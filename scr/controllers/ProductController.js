5641852827a6d6f08cdbc78183b73136583a8e3b
gandrey77 <gabrielandrey77@hotmail.com>
2020-05-01 15:35:04 -0300

    Finished for while

--- scr/controllers/ProductController.js
+++ scr/controllers/ProductController.js
@@ -1,11 +1,38 @@
 const mongoose = require('mongoose');
 
-const Procuct = mongoose.model('Product');
+const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res){
-        const products = await Procuct.find();
+        const {page = 1} = req.query;
+        const products = await Product.paginate( {}, { page: page, limit:10 });
 
         return res.json(products);
     },
+
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+        return res.json(product);
+    },
+
+    async store(req, res){
+        //Criação
+        const product = await Product.create(req.body);
+
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
+    }
 };
--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const requireDir = require('require-dir');
 // Iniciando o app
 const app = express();
 
+//Indica pra aplicação que vamos usar json para POST/GET
+app.use(express.json());
+
 // Iniciando  o DB 
 mongoose.connect('mongodb://127.0.0.1/nodeapi', 
 {useNewUrlParser: true, 
-useUnifiedTopology: true});
+useUnifiedTopology: true,
+useFindAndModify: false });
 requireDir('./scr/models');
 
-
+//Manda todas as requisições para o /routes
 app.use('/', require('./scr/routes'));
 
 
